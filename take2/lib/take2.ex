e5e7d54ca3f6b8fdc82628cbd324c591dc5fd420
Naim <skylike2904@gmail.com>
2023-12-10 13:25:02 +0600

    UP

--- take2/lib/take2.ex
+++ take2/lib/take2.ex
@@ -2,18 +2,52 @@ defmodule Take2 do
   use Application
   alias UUID
 
-  def start(_type, _args) do
-    #code:
-    Take2.main()
+  #CONSTANT VARIABLES:
+  # @pie 3.14
 
-    Supervisor.start_link([], strategy: :one_for_one)
+  def start _type, _args do
+    #CODE:
+    Take2.main
+
+    Supervisor.start_link [], strategy: :one_for_one
   end
 
   def main do
-    IO.puts(hello())
+    #LOOPS:
+    # limit = 10
+    # start = 0
+    # loop start, limit
+
+    # constant variables:
+    # IO.puts @pie
+
+    #ATOMS:
+    # name = :"Naimur Rahman"
+    # # status = :gold
+    # status = Enum.random [:gold, :silver, :bronze]
+
+    # # if status === :gold do
+    # #   IO.puts "#{name}, your status is #{status}"
+    # # else
+    # #   IO.puts "Get lost, #{name}"
+    # # end
+
+    # case status do
+    #   :gold -> IO.puts "#{name}, your status is #{status}"
+    #   :silver -> IO.puts "#{name}, your  status is #{status}"
+    #   _ -> IO.puts "Get lost!"
+    # end
+
+    #STRINGS:
+
   end
 
-  def hello do
-    UUID.uuid4()
+  #LOOPS:
+  def loop start, limit do
+    IO.puts start
+
+    if start < limit do
+      loop start + 1, limit
+    end
   end
 end
